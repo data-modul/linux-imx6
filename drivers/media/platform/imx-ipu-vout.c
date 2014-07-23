bc26f6ada1ce053975f62b47b952de13e90f313a
Zahari Doychev <zahari.doychev@linux.com>
2014-07-23 11:42:23 +0200

    [media] imx-ipu-vout: free active vb2 bufs on stop
    The currently active buffers in v4l2 driver should be hand over in
    stop_streaming.
    
    See: http://www.spinics.net/lists/linux-media/msg78804.html
    
    Signed-off-by: Zahari Doychev <zahari.doychev@linux.com>

--- drivers/media/platform/imx-ipu-vout.c
+++ drivers/media/platform/imx-ipu-vout.c
@@ -529,6 +529,7 @@ static void vout_videobuf_stop_streaming(struct vb2_queue *vq)
 {
 	struct vout_data *vout = vb2q_to_vout(vq);
 	unsigned long flags;
+	struct vout_queue *q, *tmp;
 
 	if (vout->status == VOUT_IDLE)
 		return;
@@ -544,6 +545,10 @@ static void vout_videobuf_stop_streaming(struct vb2_queue *vq)
 	}
 
 	vout->status = VOUT_IDLE;
+
+	list_for_each_entry_safe(q, tmp, &vout->idle_list, list) 
+		if (q->vb->state == VB2_BUF_STATE_ACTIVE) 
+			vb2_buffer_done(q->vb, VB2_BUF_STATE_ERROR);
 
 	spin_unlock_irqrestore(&vout->lock, flags);
 
